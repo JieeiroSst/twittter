47d5ee6bd626bc3d9307390f3eda25d07822bfe7
Alan luu <luumanhquan.91@gmail.com>
2020-08-08 22:28:03 +0700

    add views css

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,9 @@
+class RegistrationsController < Devise::RegistrationsController
+  def sign_up_params
+    params.require(:user).permit(:name,:username,:email,:password,:password_confication)
+  end
+
+  def account_update_parmas
+    params.require(:user).permit(:name,:username,:email,:password,:password_confication,:current_password)
+  end
+end
--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -1,5 +1,6 @@
 class TweeetsController < ApplicationController
   before_action :set_tweeet, only: [:show, :edit, :update, :destroy]
+  before_action :authencaticate_user!,except:[:index,:show]
 
   # GET /tweeets
   # GET /tweeets.json
