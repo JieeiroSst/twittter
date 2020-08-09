fac1e5ccb066817be3869d33446f13c1e8584b22
Alan luu <luumanhquan.91@gmail.com>
2020-08-09 22:26:14 +0700

    feats views tweeet

--- db/migrate/20200808143918_add_fields_to_users.rb
+++ db/migrate/20200808143918_add_fields_to_users.rb
@@ -1,7 +1,7 @@
 class AddFieldsToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :name, :string
-    add_column :users, :username, :string
-    add_index :users, :username ,unique:true
+    add_column :user_id,:users, :name, :string
+    add_column :user_id,:users, :username, :string
+    add_index :user_id,:users, :username ,unique:true
   end
 end
