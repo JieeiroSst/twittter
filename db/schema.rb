04e36330aeffd07ce40d88ac736d0d943d3e494f
Alan luu <luumanhquan.91@gmail.com>
2020-08-09 23:04:47 +0700

    feats views comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_153234) do
+ActiveRecord::Schema.define(version: 2020_08_09_160256) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
